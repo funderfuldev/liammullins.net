3e988f89181f6f77dbba0369235b66f3239a2067
funderfuldev <0liammullins0@gmail.com>
2026-05-17 00:07:11 -0400

    static header coming together
    working on dot hover animation with css, moving to motion instead

---
+++ liammullins-net/src/App.tsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+
+function App() 
+{
+  const randomTextChoices = [
+    "More is actually more....?",
+    "Contrast is everything......",
+    "Words words, words........",
+    "Game feel is an illusion...."
+  ];
+
+  const randomTextChoice = randomTextChoices[Math.floor(Math.random() * randomTextChoices.length)];
+
+
+  return (
+    <>
+      <div className='rotation'>
+        {/* main div for rotation */}
+
+        <span className='titleLoremIpsum'>
+          {/* banner background */}
+            <i>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie faucibus metus, et sollicitudin risus volutpat eget. Maecenas dolor ligula, iaculis id elit id, rutrum posuere metus. Nullam commodo neque ac massa tristique, ut faucibus lacus vulputate. Praesent sed lacus lorem. Quisque rutrum, dolor non viverra pulvinar, nunc metus ultricies ligula, porttitor aliquet dui metus ac leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eu eros quis elit rutrum ultricies in eu orci. Nunc sed mattis tellus. Sed tempus, enim ac volutpat pharetra, nibh velit sagittis massa, non mollis urna diam nec sem. Donec suscipit venenatis arcu. Maecenas gravida dignissim facilisis. Duis imperdiet, quam sit amet auctor tempus, nisl justo aliquet lacus, et cursus purus libero sed ante. Morbi quis elit consectetur, suscipit arcu et, fringilla diam. Suspendisse vel arcu neque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie faucibus metus, et sollicitudin risus volutpat eget. Maecenas dolor ligula, iaculis id elit id, rutrum posuere metus. Nullam commodo neque ac massa tristique, ut faucibus lacus vulputate. Praesent sed lacus lorem. Quisque rutrum, dolor non viverra pulvinar, nunc metus ultricies ligula, porttitor aliquet dui metus ac leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eu eros quis elit rutrum ultricies in eu orci. Nunc sed mattis tellus. Sed tempus, enim ac volutpat pharetra, nibh velit sagittis massa, non mollis urna diam nec sem. Donec suscipit venenatis arcu. Maecenas gravida dignissim facilisis. Duis imperdiet, quam sit amet auctor tempus, nisl justo aliquet lacus, et cursus purus libero sed ante. Morbi quis elit consectetur, suscipit arcu et, fringilla diam. Suspendisse vel arcu neque. Maecenas gravida dignissim facilisis. Duis imperdiet, quam sit amet auctor tempus, nisl justo aliquet lacus, et cursus purus libero sed ante. Morbi quis elit consectetur, suscipit arcu et, fringilla diam. Suspendisse vel arcu neque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie faucibus metus, et sollicitudin risus volutpat eget. Maecenas dolor ligula, iaculis id elit id, rutrum posuere metus. Nullam commodo neque ac massa tristique, ut faucibus lacus vulputate. Praesent sed lacus lorem. Quisque rutrum, dolor non viverra pulvinar, nunc metus ultricies ligula, porttitor aliquet dui metus ac leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Maecenas eu eros quis elit rutrum ultricies in eu orci. Nunc sed mattis tellus. Sed tempus, enim ac volutpat pharetra, nibh velit sagittis massa, non mollis urna diam nec sem. Donec suscipit venenatis arcu. Maecenas gravida dignissim facilisis. Duis imperdiet, quam sit amet auctor tempus, nisl justo aliquet lacus, et cursus purus libero sed ante. Morbi quis elit consectetur, suscipit arcu et, fringilla diam. Suspendisse vel arcu neque. 
+            </i>
+        </span>
+
+        <div className='title'>
+
+          <b>liammullins<span className='dot'></span>net</b>
+
+          <div className='randomText'>
+            {randomTextChoice}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
