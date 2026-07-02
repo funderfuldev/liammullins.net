01aead731dbc97323ddf9c2ad00c5d574b3d6e14
funderfuldev <0liammullins0@gmail.com>
2026-07-01 23:40:30 -0400

    got the header for pages looking nice on desktop
    font sizes need to be changed for mobile
    maybe have page rotate back to 0 once scrolled enough?

--- liammullins-net/src/pages/About.tsx
+++ liammullins-net/src/pages/About.tsx
@@ -1,16 +1,29 @@
 
+import './Pages.css'
+
 import {motion} from 'motion/react';
 
 export default function About() {
 
     return (
         <motion.div
-        initial={{opacity: 0, y: 25, scaleX: 0.9, scaleY: 1.1}}
-        animate={{opacity: 1, y: 0, scaleX: 1, scaleY: 1}}
-        transition={{ type: 'spring', stiffness: 500, damping: 20, mass: 1 }}
+        initial={{opacity: 0, y: 25}}
+        animate={{opacity: 1, y: 0}}
+        transition={{ type: 'spring', stiffness: 500, damping: 50, mass: 1 }}
         >
-            <p className="pageTitle"><b>about</b> is under construction.</p>
-            <p className="pageTitle">check back later!</p>
+            <div className='pageHeaderContainer'>
+                <p className='pageHeaderText'><b>biography</b>
+                <p className='pageHeaderDash'>---------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+                <p className='pageHeaderSubtext'>printer, baker, candlestick maker</p>
+                </p>
+            </div>
+
+            <div style={{height: '8vw'}}></div>
+            <div style={{rotate: '-5deg'}}>
+                <p className='pageBody'>Liam Mullins is a printmaker, digital artist, programmer, game designer, and musician. He grew up in the quiet suburbs of Richmond, Virginia. He currently attends the University of Virginia, pursuing a BA in Computer Science and Studio Art with a concentration in printmaking.</p>
+            </div>
+            
+            
         </motion.div>
     )
 }
